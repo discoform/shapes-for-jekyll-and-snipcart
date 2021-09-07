253b83a7367edd24abc90a6415dde0e5e850f646
discoform <mh97128@gmail.com>
2021-09-07 13:05:11 -0700

    update tabbed content js

--- js/tabbed-content.js
+++ js/tabbed-content.js
@@ -1,4 +1,4 @@
-function displayTab(evt, cityName) {
+function displayTab(evt, tabInfo) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
@@ -15,7 +15,7 @@ function displayTab(evt, cityName) {
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(tabInfo).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
